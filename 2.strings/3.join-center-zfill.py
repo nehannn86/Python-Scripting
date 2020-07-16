847d7f3a673dd8262d35f647dffa20360d2774dc
Cloud User <ec2-user@ip-172-31-4-75.ap-south-1.compute.internal>
2020-07-16 07:45:15 +0000

    Python Scripting

--- 2.strings/3.join-center-zfill.py
+++ 2.strings/3.join-center-zfill.py
@@ -3,7 +3,7 @@
 
 import os
 tsize=os.get_terminal_size().columns
-print('Terminal Size: ',tsize)
+print('Terminal Size is:',tsize)
 
 #getting original string
 
@@ -11,11 +11,16 @@ string='PYTHON SCRIPTING'
 print('Original String:',string)
 
 #using join() method
+
 newstring = '-'.join(string)
-print('New String created using Join() Function:', newstring)
+print('New String created using Join() Function:',newstring)
 
 #using center() method
-print('Centered String:\n', string.center(132))
+
+print('Centered String:\n',string.center(tsize))
+print('Left Sided String:\n',string.ljust(tsize))
+print('Right Sided String:\n',string.rjust(tsize))
 
 #using zfill() method
+
 print('New String created using Zfill() Function:', newstring.zfill(40))
--- 2.strings/5.index-and-find-operation.py
+++ 2.strings/5.index-and-find-operation.py
@@ -1,2 +1,51 @@
-#implemeting count on string
+#defining string.
 
+string = 'Python is an easy to learn, powerful programming language. It has efficient high-level data structures and a simple but effective approach to object-oriented programming. Python’s elegant syntax and dynamic typing, together with its interpreted nature, make it an ideal language for scripting and rapid application development in many areas on most platforms.'
+
+#defining four substrings
+
+substring1 = 'Python'
+substring2 = 'is'
+substring3 = 'it'
+substring4 = 'scripting'
+
+#printing origin string.
+
+print('string: ',string)
+print('Length of string is: ', len(string))
+
+#implementing count() operation on the above string
+
+print('\nPerforming count() operation on the above string:')
+count1 = string.count(substring1)
+count2 = string.count(substring2)
+count3 = string.count(substring3)
+count4 = string.count(substring4)
+print(f'Count of substring:"{substring1}" is: {count1}')
+print(f'Count of substring:"{substring2}" is: {count2}')
+print(f'Count of substring:"{substring3}" is: {count3}')
+print(f'Count of substring:"{substring4}" is: {count4}')
+
+#implementing index() Operation on string
+
+index1 = string.index(substring1)
+index2 = string.index(substring2)
+index3 = string.index(substring3)
+index4 = string.index(substring4)
+print('\nPerforming index() operation on the above string:')
+print(f'Index of substring "{substring1}" is: {index1}')
+print(f'Index of substring "{substring2}" is: {index2}')
+print(f'Index of substring "{substring3}" is: {index3}')
+print(f'Index of substring "{substring4}" is: {index4}')
+
+#implementing find() operation on string
+
+find1 = string.find(substring1)
+find2 = string.find(substring2)
+find3 = string.find(substring3)
+find4 = string.find(substring4)
+print('\nPerforming find() operation on the above string:')
+print(f'Find of substring "{substring1}" is: {find1}')
+print(f'Find of substring "{substring2}" is: {find2}')
+print(f'Find of substring "{substring3}" is: {find3}')
+print(f'Find of substring "{substring4}" is: {find4}')
